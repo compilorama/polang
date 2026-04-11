d9d8b0f2be9209b322d992b697655cdce996942c
Rafael Camargo <hello@rafaelcamargo.com>
2026-04-11 18:38:40 -0300

    chore: improve semantic release config

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { type: 'chore', release: 'patch' },
+      ],
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github',
